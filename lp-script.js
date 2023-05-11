0351b7fcaf7ec255241fc3620ea53b7ad264f723
matty.dev <spulermat@gmail.com>
2023-05-11 16:55:00 +0200

    add lp cards hover effect

--- lp-script.js
+++ lp-script.js
@@ -1,6 +1,10 @@
+
 const blob = document.getElementById("blob");
 
+// blob animation
 document.body.onpointermove = e => {
+   blob.style.opacity = "0.6";
+
    const { clientX, clientY } = e;
 
    blob.animate({
@@ -8,4 +12,23 @@ document.body.onpointermove = e => {
       top: `${clientY}px`
    }, { duration: 2000, fill: "forwards" });
 
+}
+
+    
+// cards hover effect
+document.getElementById("cards").onmousemove = e => {
+   blob.style.opacity = "0";
+   for (const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+         x = e.clientX - rect.left,
+         y = e.clientY - rect.top;
+      
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+   }
+}
+
+// hide blob on card hover
+document.getElementById("cards").onmouseleave = e => {
+   blob.style.opacity = "0.6";
 }
